b528e3d38afbc9825dbe5da34b310a222139a06e
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-10-24 22:11:03 +0300

    fix: add the ability to use aliases
    Delete babel-plugin-module-resolver and .babelrc. Add react-app-rewired and react-app-rewire-alias to make it possible to set up aliases.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+import Header from '@components/Header/Header';
+import Calculator from '@components/Calculator/Calculator';
+
+const AppWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
 function App() {
-    return <div className='App'></div>;
+    return (
+        <AppWrapper>
+            <Header />
+            <Calculator />
+        </AppWrapper>
+    );
 }
 
 export default App;
