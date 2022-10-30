b809ec2515fffc26c7e99cdd13a4f6bbfaa1a5d0
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-10-30 20:24:07 +0300

    refactor: put click handler outside the component
    Take out the click handler for buttons outside the buttons' component.

--- src/components/Key/KeyFC.jsx
+++ src/components/Key/KeyFC.jsx
@@ -1,61 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    loadButton,
-    loadCE,
-    loadC,
-    loadAnswer,
-} from '@store/actions/CalculatorActions';
+import clickHandler from '@utils/keyClickHandler';
+
 import { KeyWrapper } from './KeyStyles';
-import calculateExpression from '@utils/calculationFunc';
-import normalizeExpression from '@utils/normalizeFunc';
+import { useDispatch, useSelector } from 'react-redux';
 
 const KeyFC = ({ element }) => {
     const dispatch = useDispatch();
     const screenValue = useSelector((state) => state.calculator.num);
-
-    const clickHandler = (el) => {
-        const operators = ['+', '-', '/', '*', '(', ')'];
-        if (el === 'CE') {
-            dispatch(loadCE());
-        } else if (el === 'C') {
-            dispatch(loadC());
-        } else if (el === '=') {
-            const result = calculateExpression(
-                normalizeExpression(screenValue),
-            );
-            dispatch(loadAnswer(result));
-            //здесь сделать проверки на ввод и рассчеты, а также добавить в историю при валидности
-            //note: в сторе все хранится в виде "1 + 2 + 2.1", т.е. знаки отделены пробелами => при составлении массива,
-            // надо будет его разбить по пробелами, чтобы было удобно различать числа(даже десятичные) и знаки
-        } else {
-            if (operators.includes(el)) {
-                if (screenValue === '' && el !== ('(' || ')')) {
-                    //если пустая - добавить 0
-                    dispatch(loadButton(0));
-                } else if (screenValue[screenValue.length - 1] === ' ') {
-                    dispatch(loadButton(`${el} `));
-                } else {
-                    dispatch(loadButton(` ${el} `)); //каждый знак отделять пробелами
-                }
-            } else {
-                if (
-                    el === '.' &&
-                    (screenValue[screenValue.length - 1] === ' ' ||
-                        screenValue.length === 0)
-                ) {
-                    console.log(screenValue[screenValue.length - 1]);
-                    dispatch(loadButton(`0${element}`));
-                } else {
-                    dispatch(loadButton(el));
-                }
-            }
-        }
-    };
-
     return (
-        <KeyWrapper onClick={() => clickHandler(element)}>{element}</KeyWrapper>
+        <KeyWrapper
+            onClick={() => clickHandler(element, dispatch, screenValue)}
+        >
+            {element}
+        </KeyWrapper>
     );
 };
 
---
+++ src/utils/keyClickHandler.js
@@ -0,0 +1,43 @@
+import {
+    loadButton,
+    loadCE,
+    loadC,
+    loadAnswer,
+} from '@store/actions/CalculatorActions';
+import calculateExpression from './calculationFunc';
+import normalizeExpression from './normalizeFunc';
+
+export default function clickHandler(el, dispatch, screenValue) {
+    const operators = ['+', '-', '/', '*', '(', ')'];
+    if (el === 'CE') {
+        dispatch(loadCE());
+    } else if (el === 'C') {
+        dispatch(loadC());
+    } else if (el === '=') {
+        const result = calculateExpression(normalizeExpression(screenValue));
+        dispatch(loadAnswer(result));
+    } else {
+        if (operators.includes(el)) {
+            if (screenValue === '' && el !== ('(' || ')')) {
+                //если еще ничего не было введено, но уже попытались ввести
+                //арифм операцию - добавить 0 в качестве первого символа
+                dispatch(loadButton(0));
+            } else if (screenValue[screenValue.length - 1] === ' ') {
+                dispatch(loadButton(`${el} `));
+            } else {
+                dispatch(loadButton(` ${el} `)); //каждый знак (если за ним следует число) отделять пробелами
+            }
+        } else {
+            if (
+                el === '.' &&
+                (screenValue[screenValue.length - 1] === ' ' ||
+                    screenValue.length === 0)
+            ) {
+                console.log(screenValue[screenValue.length - 1]);
+                dispatch(loadButton(`0${el}`));
+            } else {
+                dispatch(loadButton(el));
+            }
+        }
+    }
+}
