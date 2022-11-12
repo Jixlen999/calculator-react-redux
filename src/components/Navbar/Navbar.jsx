cc21c143f7a30a86f3fa069b0802465affe1af13
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-11-12 21:54:29 +0300

    refactor: use Array.map for navLinks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { NavbarWrapper, NavbarLink } from './styles';
 
 function Navbar() {
+  const links = [
+    { to: '/', 'data-cy': 'homeFC', end: true, text: 'Home(FC)' },
+    { to: '/home-cc', 'data-cy': 'homeCC', end: false, text: 'Home(CC)' },
+    { to: '/settings', 'data-cy': 'settings', end: false, text: 'Settings' },
+  ];
   return (
     <NavbarWrapper>
-      <NavbarLink to="/" data-cy="homeFC" end>
-        Home(FC)
-      </NavbarLink>
-      <NavbarLink to="/home-cc" data-cy="homeCC">
-        Home(CC)
-      </NavbarLink>
-      <NavbarLink to="/settings" data-cy="settings">
-        Settings
-      </NavbarLink>
+      {links.map((link) => (
+        <NavbarLink key={link.text} to={link.to} data-cy={link['data-cy']} end={link.end ? link.end : ''}>
+          {link.text}
+        </NavbarLink>
+      ))}
     </NavbarWrapper>
   );
 }
